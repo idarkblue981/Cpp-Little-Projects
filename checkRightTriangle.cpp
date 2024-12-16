da9c017f5914ce2b37cb1cd72334455196242e31
idarkblue981 <150585644+idarkblue981@users.noreply.github.com>
2024-12-16 20:06:21 +0200

    Update checkRightTriangle.cpp

--- checkRightTriangle.cpp
+++ checkRightTriangle.cpp
@@ -1,8 +1,47 @@
 #include<iostream>
 
+// Find the biggest side
+int max(int x, int y)
+{
+    return (x > y) ? x : y;
+}
+
+int max(int x, int y, int z)
+{
+    return max(max(x, y), z);
+}
+
+// Find the middle side
+int med(int x, int y, int z)
+{
+    if((x < y && y < z) || (z < y && y < x))
+        return y;
+    else if((y < x && x < z) || (z < x && x < y))
+        return x;
+    else
+        return z;
+}
+
+// Find the smallest side
+int small(int x, int y)
+{
+    return (x < y) ? x : y;
+}
+
+int small(int x, int y, int z)
+{
+    return small(small(x, y), z);
+}
+
+// Basic check
+bool isValidTriangle(int x, int y, int z)
+{
+    return (x + y > z && x + z > y && y + z > x);
+}
+
 int main()
 {
-    std::cout << "Enter biggest side value: ";
+    std::cout << "Enter first side value: ";
     int side1{};
     std::cin >> side1;
 
@@ -14,13 +53,23 @@ int main()
     int side3{};
     std::cin >> side3;
 
-    std::cout << "The triangle with side values " << side1 << " , ";
+    if (!isValidTriangle(side1, side2, side3))
+    {
+        std::cout << "The sides do not form a valid triangle.\n";
+        return 1; // Exit if it's not ok
+    }
+
+    std::cout << "The triangle with side values " << side1 << ", ";
     std::cout << side2 << " and " << side3 << " is ";
 
-    if( (side1 * side1) != (side2 * side2) + (side3 * side3) )
-        std::cout << "not";
+    int biggest{max(side1, side2, side3)};
+    int medium{med(side1, side2, side3)};
+    int smallest{small(side1, side2, side3)};
 
-    std::cout << " a right triangle.\n";
+    if( (biggest * biggest) != (medium * medium) + (smallest * smallest) )
+        std::cout << "not ";
+
+    std::cout << "a right triangle.\n";
 
     return 0;
 }
